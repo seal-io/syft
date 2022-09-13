62fc52ae082e09fc45a266ec957c7f4a3987be39
Chapman Pendery <35637443+cpendery@users.noreply.github.com>
2022-09-13 08:20:52 -0700

    refactor: move formats from internal into syft module (#1172)

---
+++ syft/formats/cyclonedxxml/encoder.go
@@ -0,0 +1,17 @@
+package cyclonedxxml
+
+import (
+	"io"
+
+	"github.com/CycloneDX/cyclonedx-go"
+	"github.com/anchore/syft/syft/formats/common/cyclonedxhelpers"
+	"github.com/anchore/syft/syft/sbom"
+)
+
+func encoder(output io.Writer, s sbom.SBOM) error {
+	bom := cyclonedxhelpers.ToFormatModel(s)
+	enc := cyclonedx.NewBOMEncoder(output, cyclonedx.BOMFileFormatXML)
+
+	err := enc.Encode(bom)
+	return err
+}
